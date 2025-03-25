40bbe8254b341398c96aaf122f5b98a5438f946b
Ishu6129 <ishuagrawal124356@gmail.com>
2025-03-25 21:49:22 +0530

    Add PWA files

--- Mines Game/sw.js
+++ Mines Game/sw.js
@@ -1,27 +1,3 @@
-const CACHE_NAME = 'pucchu-mines-v1';
-const ASSETS = [
-'/images/mines.png',
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdn.jsdelivr.net/npm/qrcode-generator@1.4.4/qrcode.min.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+const CACHE_NAME = 'mines-game-v1';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
